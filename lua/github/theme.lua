5208ea15091aeef1162828eb6ecfadb83e39a508
wvjgsuhp <wasin.pas@hotmail.com>
2024-06-12 20:28:59 +0930

    update git signs

--- lua/github/theme.lua
+++ lua/github/theme.lua
@@ -50,9 +50,9 @@ theme.set_highlights = function()
   hl(0, "DiffChange", { fg = 'NONE', bg = c.fold_bg })
   hl(0, "DiffText", { fg = c.diff_text_fg, bg = c.diff_text_bg })
   hl(0, "DiffDelete", { fg = c.diff_delete_fg, bg = c.diff_delete_bg })
-  hl(0, "GitSignsAdd", { fg = c.ok, bg = 'NONE' })
-  hl(0, "GitSignsDelete", { fg = c.status_line_remove, bg = 'NONE' })
-  hl(0, "GitSignsChange", { fg = c.v1, bg = 'NONE' })
+  hl(0, "GitSignsAdd", { fg = c.git_add, bg = 'NONE' })
+  hl(0, "GitSignsDelete", { fg = c.git_delete, bg = 'NONE' })
+  hl(0, "GitSignsChange", { fg = c.git_change, bg = 'NONE' })
 
   -- syntax
   hl(0, "Ignore", { fg = c.fold_fg, bg = 'NONE' })
@@ -219,6 +219,9 @@ theme.set_highlights = function()
   hl(0, "NeoTreeLineNr", { link = 'Pmenu' })
   hl(0, "NeoTreeCursorLine", { link = 'PmenuSel' })
   hl(0, "NeoTreeEndOfBuffer", { link = 'PmenuBorder' })
+  hl(0, "NeoTreeGitAdded", { fg = c.ok, bg = 'NONE' })
+  hl(0, "NeoTreeGitDeleted", { fg = c.status_line_remove, bg = 'NONE' })
+  hl(0, "NeoTreeGitModified", { fg = c.v1, bg = 'NONE' })
 
   -- file-type
   hl(0, "yamlBool", { link = 'Special' })
